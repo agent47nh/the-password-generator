d478290d763c0002c506733eb356b556a0eb3b5e
Naimul Hasan <nhone4@gmail.com>
2022-02-21 19:31:54 +0100

    Few changes to README and corrected the package import name

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='thepasswordgenerator',
-    version='1.0.0',
+    version='1.0.1',
     author='Naimul Hasan',
     author_email='agent47nh9@gmail.com',
     url='https://github.com/agent47nh/the-password-generator',
